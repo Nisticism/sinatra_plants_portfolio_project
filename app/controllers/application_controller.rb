c3eecc1fecef7ebfdc01525b0e75c0aaca0662c4
nisticism <nistacular@gmail.com>
2020-04-01 22:54:34 -0700

    Made most of the routes.  Fixed some others.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < Sinatra::Base
 
   get "/" do
     if logged_in?
+      @user = User.find(session[:user_id])
       erb :account
     else
       erb :index
@@ -26,7 +27,8 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:name => params[:name], :username => params[:username], :email => params[:email], :password => params[:password])
-		if user.username.length > 0 && params[:password] != ""
+    if user.username.length > 0 && params[:password] != ""
+      user.save
 			redirect "/login"
 		else
 			redirect "/failure"
@@ -37,6 +39,58 @@ class ApplicationController < Sinatra::Base
     @user = User.find(session[:user_id])
     @plants = @user.plants
     erb :account
+  end
+
+  get '/:username/new' do
+    if logged_in?
+      @user = User.find(session[:user_id])
+      erb :new
+    else
+      redirect to '/failure'
+    end
+  end
+
+  post '/:username/show_plant' do
+    @user = User.find(session[:user_id])
+    @plant = Plant.new(species: params["species"], sprout_date: params["sprout_date"], price: params["price"], quantity: params["quantity"], user_id: @user.id)
+    if @plant.species
+      @plant.save
+      erb :'plants/show'
+    else 
+      redirect to '/failure'
+    end
+  end
+
+  get '/:username/edit/:id' do
+    if logged_in?
+      @user = User.find(session[:user_id])
+      erb :'plants/edit'
+    else
+      redirect to '/failure'
+    end
+  end
+
+  patch '/:username/edit/:id' do
+    @plant = Plant.find(params[:id])
+    @plant.update(params[:plant])
+    @plant.save
+    redirect to ':username/plants/#{@plant.id}'
+  end
+
+  get ':username/plants/:id'
+    @plant = Plant.find(params[:id])
+    erb :'plants/show'
+  end
+
+
+  # get ':username/show_plant/plant_id'
+  #   @user = User.find(session[:user_id])
+  #   @plant = @user.plants.last
+  #   erb :'plants/show'
+  # end 
+
+  post '/:username/plants' do 
+    plant = Plant.new(params[:species])
   end
 
   get '/accounts' do
@@ -50,6 +104,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(:username => params[:username])
+    #binding.pry
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			redirect "/account"
